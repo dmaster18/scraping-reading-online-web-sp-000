fed119638c00909cff35fe631e597f638e6d57cd
David Strauch <dmaster18@gmail.com>
2019-11-25 21:19:35 +0000

    Automatically backed up by Learn

--- restaurantapp.rb
+++ restaurantapp.rb
@@ -1,91 +1,7 @@
-class Restaurant
-
-  attr_accessor :name, :position, :location, :url, :head_chef, :website_url, :food_style, :best_dish, :contact, :description
-
-  @@all = []
-
-  def self.new_from_index_page(r)
-    self.new(
-      r.css("h2").text,
-      "https://www.theworlds50best.com#{r.css("a").attribute("href").text}",
-      r.css("h3").text,
-      r.css(".position").text
-      )
-  end
-
-  def initialize(name=nil, url=nil, location=nil, position=nil)
-    @name = name
-    @url = url
-    @location = location
-    @position = position
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find(id)
-    self.all[id-1]
-  end
-
-  def best_dish
-    @best_dish ||= doc.css("div.c-4.nr.nt ul:nth-child(8) li").text
-    # @best_dish ||= doc.xpath("//div[@class='c-4 nr nt']/ul[3]/li").text
-  end
-
-  def food_style
-    @food_style ||= doc.css("div.c-4.nr.nt ul:nth-child(6) li").text
-    # @food_style ||= doc.xpath("//div[@class='c-4 nr nt']/ul[2]/li").text
-  end
-
-  def contact
-    @contact ||= doc.css("div.c-4.nr.nt ul:nth-child(10) li:nth-child(1)").text.split("+").join(". Tel: +")
-    # @contact ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[1]").text.split("+").join(". Tel: +")
-  end
-
-  def head_chef
-    @head_chef ||= doc.css("div.c-4.nr.nt ul:nth-child(3) li").text.split(" (pictured)").join("")
-    # @head_chef ||= doc.xpath("//div[@class='c-4 nr nt']/ul[1]/li").text.split(" (pictured)").join("")
-  end
-
-  def website_url
-    @website_url ||= doc.css("div.c-4.nr.nt ul:nth-child(10) li:nth-child(2) a").text
-    # @website_url ||= doc.xpath("//div[@class='c-4 nr nt']/ul[4]/li[2]/a").text
-  end
-
-  def description
-    @description ||= doc.css("div.c-8.nl.nt > p:nth-child(6)").text
-    # @description ||= doc.xpath("//div[@class='c-8 nl nt']/p[3]").text
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
-end
-
-
-class Scraper
-
-  def get_page
-    Nokogiri::HTML(open("https://www.theworlds50best.com/list/1-50-winners"))
-  end
-
-  def scrape_restaurants_index
-     self.get_page.css("div#t1-50 li")
-  end
-
-  def make_restaurants
-    scrape_restaurants_index.each do |r|
-      Restaurant.new_from_index_page(r)
-    end
-  end
-end
-
-class CLI
+class WorldsBestRestaurants::CLI
 
   def call
-    Scraper.new.make_restaurants
+    WorldsBestRestaurants::Scraper.new.make_restaurants
     puts "Welcome to the 50 Best Restaurants in the World"
     start
   end
@@ -101,7 +17,7 @@ class CLI
     puts "What restaurant would you like more information on?"
     input = gets.strip
 
-    restaurant = Restaurant.find(input.to_i)
+    restaurant = WorldsBestRestaurants::Restaurant.find(input.to_i)
 
     print_restaurant(restaurant)
 
@@ -143,10 +59,9 @@ class CLI
     puts ""
     puts "---------- Restaurants #{from_number} - #{from_number+9} ----------"
     puts ""
-    Restaurant.all[from_number-1, 10].each.with_index(from_number) do |restaurant, index|
+    WorldsBestRestaurants::Restaurant.all[from_number-1, 10].each.with_index(from_number) do |restaurant, index|
       puts "#{index}. #{restaurant.name} - #{restaurant.location}"
     end
   end
 
 end
-
