3de70f3fb8dc0a234c0987c6eb012e302cc1f78d
David Strauch <dmaster18@gmail.com>
2019-11-25 21:21:37 +0000

    Automatically backed up by Learn

--- restaurantapp.rb
+++ restaurantapp.rb
@@ -132,7 +132,7 @@ class WorldsBestRestaurants::Restaurant
   end
 end
 
-class Scraper
+class WorldsBestRestaurants::Scraper
 
   def get_page
     Nokogiri::HTML(open("https://www.theworlds50best.com/list/1-50-winners"))
